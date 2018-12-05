59cb6eee147815ab94c3a50c1e1d33474a03443c
linkDCat <link.d.chiang@gmail.com>
2018-12-05 15:46:05 +0800

    add static_root

--- planc/settings.py
+++ planc/settings.py
@@ -146,6 +146,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static_dist')
+
 AUTH_USER_MODEL = 'catauth.Front_User'
 
 MEDIA_URL = '/media/'
